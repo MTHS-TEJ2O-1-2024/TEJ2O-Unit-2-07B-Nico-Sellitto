8a9e268869c11f61473d4cc120571770afa70dfd
Nico-Sellitto <nico.sellitto@mths.ca>
2024-10-01 10:49:45 -0400

    changed text

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Nico Sellitto
+ * Created on: Oct 2024
+ * This program is Cookie Clicker
 */
 
-basic.showString('Hello, World!')
+
