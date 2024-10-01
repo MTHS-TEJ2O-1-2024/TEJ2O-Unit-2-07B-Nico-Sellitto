83e7c00de7446f2061dfc3e0be56f7404dd14daf
Nico-Sellitto <nico.sellitto@mths.ca>
2024-10-01 11:08:01 -0400

    programmed cookie clicker

--- main.ts
+++ main.ts
@@ -5,4 +5,25 @@
  * This program is Cookie Clicker
 */
 
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
 
+let CookieNumber = 0
+
+// Shows the number of cookies clicked
+input.onButtonPressed(Button.A, function () {
+    
+    basic.clearScreen()
+    CookieNumber += 1
+    basic.showNumber(CookieNumber)
+    
+})
+
+// Resets the number of cookies collected
+input.onButtonPressed(Button.B, function () {
+    
+    basic.clearScreen()
+    CookieNumber = 0
+    basic.showNumber(CookieNumber)
+
+})
